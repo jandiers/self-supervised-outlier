e9e364eca475fbb59f2dc8d20cb7d0079037b05f
JanDiers <jan.diers@uni-jena.de>
2020-08-19 11:54:22 +0200

    added setup for distribution of package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='noisy_outlier',
+    version='0.1.0',
+    author='Jan Diers',
+    author_email='jan.diers@uni-jena.de',
+    description='Self-Supervised Learning for Outlier Detection.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JanDiers/self-supervised-outlier',
+    packages=['noisy_outlier', 'noisy_outlier.model', 'noisy_outlier.hyperopt'],
+    classifiers=[
+                  "Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent",
+              ],
+    python_requires='>=3.6',
+)
